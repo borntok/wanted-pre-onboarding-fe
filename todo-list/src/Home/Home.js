b22a30dd9449fa8565c3d94688b13e24f1c8c41a
JU Yerin <yerin0823@naver.com>
2022-10-10 17:12:58 +0900

    email, pw 유효성 검사

--- todo-list/src/Home/Home.js
+++ todo-list/src/Home/Home.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import styles from "./Home.module.css";
-import InputForm from "./InputForm";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailCheck, setEmailCheck] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState(false);
+
+  function checkEmail(e) {
+    const emailRegex =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+    setEmail(e.target.value);
+
+    if (!emailRegex.test(e.target.value)) {
+      setEmailCheck(false);
+      console.log("잘못된 이메일");
+    } else {
+      setEmailCheck(true);
+      console.log("굿");
+    }
+  }
+
+  function checkPassword(e) {
+    const passwordRegex = /^[A-Za-z0-9]{8,20}$/;
+
+    setPassword(e.target.value);
+
+    if (!passwordRegex.test(e.target.value)) {
+      setPasswordCheck(false);
+      console.log("잘못된 비밀번호", password);
+    } else {
+      setPasswordCheck(true);
+      console.log("비밀번호 486", password);
+    }
+  }
+
   function signUp(e) {
     e.preventDefault();
     console.log("회원가입");
@@ -16,12 +51,38 @@ export default function Home() {
     <main>
       <form className={styles.formContainer}>
         <div className={styles.inputForm}>
-          <InputForm />
+          <div>
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="이메일을 입력해 주세요."
+              onChange={checkEmail}
+              required
+            />
+          </div>
+          <div>
+            <input
+              className={styles.input}
+              type="password"
+              minLength="8"
+              placeholder="비밀번호를 입력해 주세요."
+              onChange={checkPassword}
+              required
+            />
+          </div>
         </div>
-        <button onClick={signUp} className={styles.button}>
+        <button
+          onClick={signUp}
+          className={styles.button}
+          disabled={!emailCheck || !passwordCheck}
+        >
           회원가입
         </button>
-        <button onClick={signIn} className={styles.button}>
+        <button
+          onClick={signIn}
+          className={styles.button}
+          disabled={!emailCheck || !passwordCheck}
+        >
           로그인
         </button>
       </form>
--- todo-list/src/Home/InputForm.js
+++ todo-list/src/Home/InputForm.js
@@ -1,15 +1,29 @@
 import styles from "./InputForm.module.css";
 
 export default function InputForm() {
+  function checkEmail(email) {
+    const emailRegex =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    console.log(email.target.value, emailRegex.test(email.target.value));
+  }
+
   return (
     <>
       <div>
-        <input className={styles.input} placeholder="이메일을 입력해 주세요." />
+        <input
+          className={styles.input}
+          type="email"
+          placeholder="이메일을 입력해 주세요."
+          onBlur={checkEmail}
+        />
       </div>
       <div>
         <input
           className={styles.input}
+          type="password"
+          minLength="8"
           placeholder="비밀번호를 입력해 주세요."
+          required
         />
       </div>
     </>
